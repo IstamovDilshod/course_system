927b8568506e5330f9d0db4ff52b901c61207491
Dilshod <istamov725dilshod@gmail.com>
2026-04-27 20:18:28 +0500

    Everything changed

--- course_app/urls.py
+++ course_app/urls.py
@@ -2,6 +2,11 @@ from django.urls import path, include
 from rest_framework_nested import routers
 from rest_framework_simplejwt.views import TokenRefreshView
 from .serializers import MyTokenObtainPairView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path
+
 from .views import (
     CourseViewSet, RegisterView, EnrollView,
     MyCourseView, UserProfileView, LessonViewSet, ReviewViewSet
@@ -15,6 +20,16 @@ router.register(r'courses', CourseViewSet, basename='course')
 course_router = routers.NestedDefaultRouter(router, r'courses', lookup='course')
 course_router.register(r'lessons', LessonViewSet, basename='course-lessons')
 course_router.register(r'reviews', ReviewViewSet, basename='course-reviews')
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     # Auth
@@ -32,4 +47,6 @@ urlpatterns = [
     # Courses + nested (lessons, reviews)
     path('', include(router.urls)),
     path('', include(course_router.urls)),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
 ]
